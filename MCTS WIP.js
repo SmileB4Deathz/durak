331a0c53b6e7bbf01bd315ef8344097292446b25
SmileB4Deathz <80955493+SmileB4Deathz@users.noreply.github.com>
2023-09-04 17:36:54 +0200

    Update MCTS WIP.js

--- MCTS WIP.js
+++ MCTS WIP.js
@@ -1,194 +1,204 @@
+var $jscomp=$jscomp||{};$jscomp.scope={};$jscomp.arrayIteratorImpl=function(a){var b=0;return function(){return b<a.length?{done:!1,value:a[b++]}:{done:!0}}};$jscomp.arrayIterator=function(a){return{next:$jscomp.arrayIteratorImpl(a)}};$jscomp.makeIterator=function(a){var b="undefined"!=typeof Symbol&&Symbol.iterator&&a[Symbol.iterator];if(b)return b.call(a);if("number"==typeof a.length)return $jscomp.arrayIterator(a);throw Error(String(a)+" is not an iterable or ArrayLike");};
+$jscomp.arrayFromIterator=function(a){for(var b,c=[];!(b=a.next()).done;)c.push(b.value);return c};$jscomp.arrayFromIterable=function(a){return a instanceof Array?a:$jscomp.arrayFromIterator($jscomp.makeIterator(a))};
+var MCTSNode=function(a,b){this.moves=a;this.parent=b;this.wins=this.visits=0;this.numUnexpandedMoves=a.length;this.children=Array(this.numUnexpandedMoves).fill(null)},MCTS=function(a,b,c,d){this.game=a;this.player=b;this.iterations=c;this.exploration=d;void 0==this.iterations&&(this.iterations=500);void 0==this.exploration&&(this.exploration=1.41)};
+MCTS.prototype.selectMove=function(){console.time();for(var a=this.game.cloneState(),b=this.game.moves(),c=new MCTSNode(b,null),d=0;d<this.iterations;d++){this.game.setState(a);var e=this.game.cloneState();this.game.setState(e);e=this.selectNode(c);this.game.gameOver()&&this.game.winner()!=this.player&&-1!=this.game.winner()&&(e.parent.wins=Number.MIN_SAFE_INTEGER);e=this.expandNode(e);this.playout(e);var g=this.game.winner()==this.player?1:0;this.backprop(e,g)}b=b[this.getBestChildIndex(c)];this.game.setState(a);
+console.log(c);a=this.getPv(c);console.timeEnd();return{move:b,pv:a,stats:{wins:c.wins,visits:c.visits}}};MCTS.prototype.selectNode=function(a){for(var b=this.exploration;0==a.numUnexpandedMoves;){var c=-Infinity,d=-1,e=a.visits,g;for(g in a.children){var f=a.children[g],h=f.visits;f=this.game.getPlayerTurn()==this.player?f.wins:-f.wins;h=this.computeUCB(f,h,b,e);h>c&&(c=h,d=g)}c=this.game.moves();this.game.playMove(c[d]);a=a.children[d];if(this.game.gameOver())break}return a};
+MCTS.prototype.expandNode=function(a){if(this.game.gameOver())return a;var b=this.game.moves(),c=this.selectRandomUnexpandedChild(a);void 0==b[c]&&console.log(this.game);this.game.playMove(b[c]);b=this.game.moves();b=new MCTSNode(b,a);a.children[c]=b;--a.numUnexpandedMoves;return b};MCTS.prototype.playout=function(){for(;!this.game.gameOver();){var a=this.greedyMove(this.game);this.game.playMove(a)}return this.game.winner()};
+MCTS.prototype.backprop=function(a,b){for(;null!=a;)a.visits+=1,a.wins+=b,a=a.parent};MCTS.prototype.selectRandomUnexpandedChild=function(a){var b=Math.floor(Math.random()*a.numUnexpandedMoves),c=-1,d;for(d in a.children)if(null==a.children[d]&&(c+=1),c==b)return d};MCTS.prototype.computeUCB=function(a,b,c,d){return a/b+c*Math.sqrt(Math.log(d)/b)};MCTS.prototype.getBestChildIndex=function(a){var b=-Infinity,c=-1,d;for(d in a.children){var e=a.children[d];null!=e&&e.wins>b&&(b=e.wins,c=d)}return c};
+MCTS.prototype.getPv=function(a){for(var b=[];0!=a.children.length;){var c=this.getBestChildIndex(a);b.push(a.moves[c]);a=a.children[c];if(void 0==a)break}return b};MCTS.prototype.lowestRankCard=function(a){var b=function(c){if(1>=c.length)return c;for(var d=c[0],e=[],g=[],f=1;f<c.length;f++)c[f].Value<d.Value?e.push(c[f]):g.push(c[f]);return[].concat($jscomp.arrayFromIterable(b(e)),[d],$jscomp.arrayFromIterable(b(g)))};return b(a)[0]};
+MCTS.prototype.greedyMove=function(){var a=this.game.moves();return 1===a.length?a[0]:this.getGreedyMove()};MCTS.prototype.getGreedyMove=function(){var a=this.game.moves().filter(function(d){return"object"===typeof d}),b=this.game.getTrump(),c=a.filter(function(d){return d.Type!=b});return 0===c.length?this.lowestRankCard(a):this.lowestRankCard(c)};
 
-class MCTSNode {
-    constructor(moves, parent) {
-        this.moves = moves
-        this.parent = parent
-        this.visits = 0
-        this.wins = 0
-        this.numUnexpandedMoves = moves.length
-        this.children = new Array(this.numUnexpandedMoves).fill(null) //temporary store move for debugging purposes
-    }
-}
+//------------------------------------------------------------------------------------------------------------------------------------------------
 
+class Durak {
 
-class MCTS {
-    constructor(game, player, iterations, exploration) {
-        this.game = game
-        this.player = player
-        this.iterations = iterations
-        this.exploration = exploration
-
-        if (this.iterations == undefined) {
-            this.iterations = 500
+    constructor(playerCards, playerTurn, trump, cardsOnTable, attackCards = [], lowerTrump = null, deck = []) {
+        this.state = {
+            playerCards: playerCards,
+            playerTurn: playerTurn,
+            trump: trump,
+            cardsOnTable: cardsOnTable,
+            attackCards: attackCards,
+            deck: deck,
+            lowerTrump: lowerTrump,
+            endGame: false,
+            winner: -1,
+            gameOver: false,
+            isTaking: false
         }
-        if (this.exploration == undefined) {
-            this.exploration = 1.41
-        }
     }
 
-    selectMove() {
-        const originalState = this.game.cloneState()
-        const possibleMoves = this.game.moves()
-        const root = new MCTSNode(possibleMoves, null)
+    getState() { return this.state }
 
-        for (let i = 0; i < this.iterations; i++) {
-            this.game.setState(originalState)
-            const clonedState = this.game.cloneState()
-            this.game.setState(clonedState)
+    setState(state) { this.state = state }
 
-            let selectedNode = this.selectNode(root)
-            //if selected node is terminal and we lost, make sure we never choose that move
-            if (this.game.gameOver()) {
-                if (this.game.winner() != this.player && this.game.winner() != -1) {
-                    selectedNode.parent.wins = Number.MIN_SAFE_INTEGER
+    getPlayerTurn() { return this.state.playerTurn }
+
+    getTrump() { return this.state.trump }
+
+    cloneState() {
+        return structuredClone(this.state);
+    }
+
+    static sameCard(c1, c2) {
+        if (c1.Value === c2.Value && c1.Type === c2.Type) {
+            return true;
+        }
+        return false;
+    }
+
+    moves() {
+        let moves = [];
+        if (this.state.gameOver)
+            return moves;
+        //first attack
+        if (this.state.cardsOnTable.length === 0) {
+            moves = this.state.playerCards[this.state.playerTurn];
+        }
+        //continue attack
+        else if (this.state.attackCards.length === 0) {
+            if (this.state.isTaking) {
+                let oppIndex = this.state.playerTurn === 0 ? 1 : 0;
+                if (this.state.cardsOnTable.length >= this.state.playerCards[oppIndex].length)
+                    return (["pass"]);
+            }
+            moves = this.state.playerCards[this.state.playerTurn].filter(card => this.state.cardsOnTable.some(tc => tc.Value === card.Value));
+            moves.push("pass");
+        }
+        //defence
+        else if (this.state.attackCards.length !== 0) {
+            //if attack card is not a trump
+            if (this.state.attackCards[0].Type !== this.state.trump) {
+                for (let i = 0; i < this.state.playerCards[this.state.playerTurn].length; i++) {
+                    let cardType = this.state.playerCards[this.state.playerTurn][i].Type;
+                    let cardValue = this.state.playerCards[this.state.playerTurn][i].Value;
+                    if ((cardType === this.state.attackCards[0].Type && cardValue > this.state.attackCards[0].Value) || cardType === this.state.trump) {
+                        moves.push(this.state.playerCards[this.state.playerTurn][i]);
+                    }
                 }
             }
-            let expandedNode = this.expandNode(selectedNode)
-            this.playout(expandedNode)
-
-            let reward;
-            if (this.game.winner() == this.player) { reward = 1 }
-            else { reward = 0 }
-            this.backprop(expandedNode, reward)
-        }
-
-        //choose move with most wins
-        let bestMove = possibleMoves[this.getBestChildIndex(root)];
-
-        this.game.setState(originalState)
-        console.log(root);
-        const pv = this.getPv(root);
-        return {move: bestMove, pv: pv, stats: {wins: root.wins, visits: root.visits}};
-    }
-    selectNode(root) {
-
-        const c = this.exploration
-
-        while (root.numUnexpandedMoves == 0) {
-            let maxUBC = -Infinity
-            let maxIndex = -1
-            let Ni = root.visits
-            for (let i in root.children) {
-                const child = root.children[i]
-                const ni = child.visits
-                const wi = (this.game.getPlayerTurn() == this.player) ? child.wins : -(child.wins)
-                //const wi = child.wins;
-                const ubc = this.computeUCB(wi, ni, c, Ni)
-                if (ubc > maxUBC) {
-                    maxUBC = ubc
-                    maxIndex = i
+            //if attack card is a trump
+            else {
+                for (let i = 0; i < this.state.playerCards[this.state.playerTurn].length; i++) {
+                    let cardType = this.state.playerCards[this.state.playerTurn][i].Type;
+                    let cardValue = this.state.playerCards[this.state.playerTurn][i].Value;
+                    if (cardType === this.state.attackCards[0].Type && cardValue > this.state.attackCards[0].Value)
+                        moves.push(this.state.playerCards[this.state.playerTurn][i]);
                 }
             }
-            const moves = this.game.moves()
-            this.game.playMove(moves[maxIndex])
-
-            root = root.children[maxIndex]
-            if (this.game.gameOver()) {
-                return root
-            }
+            moves.push("take");
         }
-        return root
+        return moves;
     }
 
-    expandNode(node) {
-        if (this.game.gameOver()) {
-            return node
-        }
-        let moves = this.game.moves()
-        const childIndex = this.selectRandomUnexpandedChild(node)
-        this.game.playMove(moves[childIndex])
-
-        moves = this.game.moves()
-        const newNode = new MCTSNode(moves, node)
-        node.children[childIndex] = newNode
-        node.numUnexpandedMoves -= 1
-
-        return newNode
-    }
-
-    playout() {
-        while (!this.game.gameOver()) {
-            const moves = this.game.moves()
-            const randomChoice = Math.floor(Math.random() * moves.length)
-            this.game.playMove(moves[randomChoice])
-        }
-        return this.game.winner()
-    }
-    backprop(node, reward) {
-        while (node != null) {
-            node.visits += 1
-            node.wins += reward
-            node = node.parent
-        }
-    }
-
-    // returns index of a random unexpanded child of node
-    selectRandomUnexpandedChild(node) {
-        const choice = Math.floor(Math.random() * node.numUnexpandedMoves) //expand random nth unexpanded node
-        let count = -1
-        for (let i in node.children) {
-            const child = node.children[i]
-            if (child == null) {
-                count += 1
-            }
-            if (count == choice) {
-                return i
-            }
-        }
-    }
-
-    computeUCB(wi, ni, c, Ni) {
-        return (wi / ni) + c * Math.sqrt(Math.log(Ni) / ni)
-    }
-
-    getBestChildIndex(node){
-        let maxWins = -Infinity
-        let maxIndex = -1
-        for (let i in node.children) {
-            const child = node.children[i]
-            if (child == null) { continue }
-            if (child.wins > maxWins) {
-                maxWins = child.wins
-                maxIndex = i
-            }
-        }
-        return maxIndex;
-    }
-
-    getPv(root) {
-        const pv = [];
-        while (root.children.length != 0) {
-            let maxIndex = this.getBestChildIndex(root);
-            pv.push(root.moves[maxIndex]);
-            root = root.children[maxIndex];
-            if (root == undefined){
+    playMove(move) {
+        switch (move) {
+            case "pass":
+                if (this.state.isTaking) {
+                    let oppIndex = this.state.playerTurn === 0 ? 1 : 0;
+                    this.state.cardsOnTable.forEach(card => this.state.playerCards[oppIndex].push(card));
+                    this.state.isTaking = false;
+                    this.takeCards(this.state.playerTurn);
+                }
+                else {
+                    this.takeCards(this.state.playerTurn);
+                    this.state.playerTurn = (this.state.playerTurn === 0) ? 1 : 0;
+                }
+                this.state.cardsOnTable = [];
+                this.state.attackCards = [];
                 break;
+            case "take":
+                this.state.attackCards = [];
+                this.state.isTaking = true;
+                this.state.playerTurn = (this.state.playerTurn === 0) ? 1 : 0;
+                break;
+            default:
+                if (this.state.isTaking) {
+                    this.state.cardsOnTable.push(move);
+                    this.state.playerCards[this.state.playerTurn] = this.state.playerCards[this.state.playerTurn].filter(card => !Durak.sameCard(card, move));
+                    if (this.state.playerCards[this.state.playerTurn].length === 0 && this.state.endGame)
+                        this.state.gameOver = true;
+                    return;
+                }
+
+                this.state.cardsOnTable.push(move);
+                this.state.playerCards[this.state.playerTurn] = this.state.playerCards[this.state.playerTurn].filter(card => !Durak.sameCard(card, move));
+                if (this.state.playerCards[this.state.playerTurn].length === 0 && this.state.endGame) {
+                    this.state.winner = this.state.playerTurn;
+                    this.state.gameOver = true;
+                    return;
+                }
+
+                //multiple attack cards on the table
+                if (this.state.attackCards.length === 0) {
+                    this.state.attackCards.push(move);
+                    this.state.playerTurn = (this.state.playerTurn === 0) ? 1 : 0;
+                    return;
+                }
+                else if (this.state.attackCards.length === 1) {
+                    this.state.attackCards.shift();
+                    this.state.playerTurn = (this.state.playerTurn === 0) ? 1 : 0;
+                    return;
+                }
+                this.state.attackCards.shift();
+                break;
+        }
+    }
+
+    takeCards(first) {
+        if (!first) {
+            if (this.state.playerCards[0].length < 6)
+                take(0, this.state);
+            if (this.state.playerCards[1].length < 6)
+                take(1, this.state);
+            return;
+        }
+
+        if (this.state.playerCards[1].length < 6)
+            take(1, this.state);
+        if (this.state.playerCards[0].length < 6)
+            take(0, this.state);
+
+        function take(player, state) {
+            if (state.endGame)
+                return;
+            const nrCardsToTake = 6 - state.playerCards[player].length;
+            for (let i = 0; i < nrCardsToTake; i++) {
+                //const randomCard = state.deck[Math.floor(Math.random() * state.deck.length)];
+                const randomCard = state.deck[0];
+                state.playerCards[player].push(randomCard);
+                state.deck.shift();
+                if (state.deck.length === 0) {
+                    if (state.lowerTrump == null){
+                        state.endGame = true;
+                        return;
+                    }
+                    state.deck.push(state.lowerTrump)
+                    state.lowerTrump = null;                   
+                }
             }
         }
-        return pv;
     }
 
-    greedyMove(game){
-        function Move(moves){
-            if (moves.length === 1)
-                return moves[1];
-            return getGreedyMove(game)
-        }
-
-        function getGreedyMove(game){
-            const cards = game.moves().filter(card => (card => typeof card === "object"));
-            const trump = game.getTrump();
-            const noTrumpCards = cards.filter(card => card.Type != trump);
-            if (noTrumpCards.length === 0)
-                return 
-        }
-
-        return Move(game.moves());
+    gameOver() {
+        return this.state.gameOver;
     }
-
-    sortByRank(cards){
-        
-    }
+    winner() { return this.state.winner }
 }
 
-module.exports = { MCTS }
+
+
+class Card {
+    constructor(Value, Type) {
+        this.Value = Value;
+        this.Type = Type;
+    }
+
+    getValue() {
+        return this.Value;
+    }
+
+    getType() {
+        return this.Type;
+    }
+}
